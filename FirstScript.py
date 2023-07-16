bafa926d58d34a5f6595407590302c6a931b07ca
Tahir Salinawala <tahirsalinawala@Admins-MacBook-Air.local>
2023-07-16 15:38:09 +0530

    latest push

--- FirstScript.py
+++ FirstScript.py
@@ -1,24 +1,25 @@
-import os
 import shutil
+from tqdm import tqdm
+
+def copy_file(source, destination):
+    with tqdm(total=100, ncols=80, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}') as pbar:
+        def progress_callback(num_bytes):
+            pbar.update(num_bytes / 1024)  # Update progress bar with kilobytes transferred
+
+        try:
+            shutil.copy2(source, destination, progress_callback)
+            pbar.set_description("Copying")  # Update progress bar description
+            pbar.refresh()
+            print("Copied successfully")
+            return True
+        except Exception as e:
+            print("Unsuccessful")
+            print(f"Error: {str(e)}")
+            return False
 
 
-source=input("enter the source path:")
+if __name__ == '__main__':
+    source_by_user = input("Enter the source path: ")
+    destination_by_user = input("Enter the destination path: ")
 
-destination=input("enter the destination path:")
-
-
-
-
-# Copy the content of
-# source to destination
-if shutil.copyfile(source, destination):
-    print("copied sucessfully")
-else:
-    print("unsucessfull")    
-
-
-
-
-
-
-
+    copy_file(source=source_by_user, destination=destination_by_user)
